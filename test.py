e362f15e5ff76de1d51c041f2f5b0bc072b3df5a
Diflent <steam.ilya@gmail.com>
2026-09-23 22:15:25 +0300

    fix recursive tests

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import unittest
 
+from main import gcd_recursive
+
+
 class TestGCD(unittest.TestCase):
     def test_recursive(self):
         self.assertEqual(gcd_recursive(48, 18), 6)
@@ -10,3 +13,7 @@ class TestGCD(unittest.TestCase):
         self.assertEqual(gcd_recursive(-48, -18), 6)
         self.assertEqual(gcd_recursive(1, 1), 1)
         self.assertEqual(gcd_recursive(0, 0), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
